62270247700445a8165c8623592897ea40fa5a7a
lHeidbreder <lukas@boeke.email>
2022-05-10 11:41:00 +0200

    fixed restructuring

--- lib/DobotDllType.py
+++ lib/DobotDllType.py
@@ -1,6 +1,8 @@
 from ctypes import *
 import time,  platform
 import os
+import sys
+sys.path.append("./lib")
 
 def enum(**enums):
     return type("Enum", (), enums)
@@ -588,9 +590,9 @@ def load():
     if platform.system() == "Windows":
         print("您用的dll是64位，为了顺利运行，请保证您的python环境也是64位")
         print("python环境是：",platform.architecture())
-        return CDLL("./DobotDll.dll",  RTLD_GLOBAL)
+        return CDLL("./lib/DobotDll.dll",  RTLD_GLOBAL)
     elif platform.system() == "Darwin":
-        return CDLL("./libDobotDll.dylib",  RTLD_GLOBAL)
+        return CDLL("./lib/libDobotDll.dylib",  RTLD_GLOBAL)
     elif platform.system() == "Linux":
         return cdll.loadLibrary("libDobotDll.so")
 
