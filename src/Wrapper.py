74e24a145c732f4be27084173cfcaa59c6535afa
Thomas Pietzka <t.pietzka@westfaliaeurope.com>
2022-05-25 13:46:05 +0200

    Kleine Anpassung am Wrapper zur besseren Funktionalität

--- src/Wrapper.py
+++ src/Wrapper.py
@@ -3,7 +3,7 @@ from importlib import import_module
 class Wrapper:
 	DobotAPI = None
 	__DobotID = -1
-
+	__isConnected = False
 
 	def __init__(self, playerID: int, comPort: str):
 		self.__DobotID = playerID
@@ -22,11 +22,11 @@ class Wrapper:
 		self.DobotAPI = self.DobotDLL.load() #Dll laden und Verwendung des CDLL Objekts
 		
 		#Verbindung zu Dobot herstellen
-		state = self.DobotDLL.ConnectDobot(self.DobotAPI, comPort, 115200)[0]
+		self.__isConnected = self.DobotDLL.ConnectDobot(self.DobotAPI, comPort, 115200)[0]
 
-		print("Connect status of dobot", playerID, CON_STR1[state])
+		print("Connect status of dobot", playerID, CON_STR1[self.__isConnected])
 
-		if (state == self.DobotDLL.DobotConnect.DobotConnect_NoError):
+		if (self.__isConnected == self.DobotDLL.DobotConnect.DobotConnect_NoError):
 			print("Setting up Command Queue")
 			self.DobotDLL.SetQueuedCmdClear(self.DobotAPI)                        #Clean Command Queued
 			self.DobotDLL.SetQueuedCmdStartExec(self.DobotAPI)                    #Befehlswarteschlange starten
@@ -36,34 +36,41 @@ class Wrapper:
 
 
 	def Home(self):
-		print("Homing...")    
-		self.DobotDLL.SetHOMEParams(self.DobotAPI, 250, 0, 50, 0, isQueued = 1)
-		self.DobotDLL.SetHOMECmd(self.DobotAPI, 0, isQueued = 1)
+		if (self.__isConnected == self.DobotDLL.DobotConnect.DobotConnect_NoError):
+			print("Homing...")    
+			self.DobotDLL.SetHOMEParams(self.DobotAPI, 250, 0, 50, 0, isQueued = 1)
+			self.DobotDLL.SetHOMECmd(self.DobotAPI, 0, isQueued = 1)
 
-		self.WaitForMotionComplete()
-		pass
-
-
-	''' 
-	Moving the fastest way to the provided position.
-	'''
-	def MoveTo(self, x: int, y: int, z: int, r: int, l: int):
-		print("Moving Dobot", self.__DobotID, "to coordinates", x, y, z, r, l)
-
-		#TODO add move command
-		self.DobotDLL.SetPTPWithLCmdEx(self.DobotDLL, 0, x, y, z, r, l, isQueued=1)
-
-		self.WaitForMotionComplete()
-		pass
+			self.WaitForMotionComplete()
+		
+		else:
+			print("Dobot not connected! Can't home.")
 
 
-	def MoveToInLine(self, x: int, y: int, z: int, r: int, l: int):
-		print("Moving Dobot", self.__DobotID, "in a straight line to", x, y, z, r, l)
+	def MoveTo(self, x: int, y: int, z: int, r: int):
+		if (self.__isConnected == self.DobotDLL.DobotConnect.DobotConnect_NoError):
+			print("Moving Dobot", self.__DobotID, "to coordinates", x, y, z, r)
 
-		# TODO add move command
+			#TODO add move command
+			self.DobotDLL.SetPTPCmdEx(self.DobotDLL, 0, x, y, z, r, isQueued=1)
 
-		self.WaitForMotionComplete()
-		pass
+			self.WaitForMotionComplete()
+		
+		else:
+			print("Dobot not conncted! Can't move.")
+
+
+	def MoveToInLine(self, x: int, y: int, z: int, r: int):
+		if (self.__isConnected == self.DobotDLL.DobotConnect.DobotConnect_NoError):
+			print("Moving Dobot", self.__DobotID, "in a straight line to", x, y, z, r)
+
+			# TODO add move command
+			print("Command not implemented")
+
+			self.WaitForMotionComplete()
+		
+		else:
+			print("Dobot not conncted! Can't move.")
 
 
 	def WaitForMotionComplete(self):
@@ -74,7 +81,9 @@ class Wrapper:
 
 
 	def Disconnect(self):    
-		#TODO disconnect dobot
-		self.DobotDLL.DisconnectDobot(self.DobotAPI)
-		print("Successfully disconnected Dobot", self.__DobotID)
-		pass
+		if (self.__isConnected == self.DobotDLL.DobotConnect.DobotConnect_NoError):
+			self.DobotDLL.DisconnectDobot(self.DobotAPI)
+			print("Successfully disconnected Dobot", self.__DobotID)
+		
+		else:
+			print("Dobot not conncted! Can't disconnect.")
